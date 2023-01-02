54d66830997b3b7bec59ad12648572a4de6e29c4
Leah Itzinger <litzinger@integralytic.com>
2023-01-02 12:27:34 +0200

    changes for 02,01

---
+++ analysis_fun.py
@@ -0,0 +1,17 @@
+
+
+
+def clean(df,col):
+    df['children']=df['children'].fillna(0)
+
+def fun(df,colname):
+    colname1 = colname + '_1'
+    colname1=df.reindex(df.index.repeat(df[colname]))
+    colname1[colname]=colname1[colname].replace(colname1[colname].unique(),colname)
+    colname1.rename(columns={colname: "people"}, inplace=True)
+    return colname1
+   
+
+
+
+
