7e6a870e086f32366cf593be82e57ab01235a13e
Leah Itzinger <litzinger@integralytic.com>
2023-01-03 09:28:37 +0200

    added all file

---
+++ old/guided_project_function.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+def catplot(a,b):
+    sns.catplot(data=a, col='people',x='arrival_date_year',kind='count',hue='hotel')
+    plt.suptitle(b, y=1.1)
+
+
+
+def catplotpoint(a,b):
+    sns.catplot(data=a ,x=b ,y='is_canceled', kind='point',hue='hotel')
+    plt.suptitle('percent of canceled {} arrivals per hotel'.format(b), y=1.1)
+
