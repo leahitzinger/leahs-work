fb42bbdd26596f4d1e1d27b8bf99a9d2db5f379a
Leah Itzinger <litzinger@integralytic.com>
2023-01-04 12:37:29 +0200

    fun. wash_laundry

---
+++ opp/1.py
@@ -0,0 +1,27 @@
+def set_settings(temp,spinspeed,cycle):
+    print('my temperture is' , temp)
+    print('my spinspeed is' , spinspeed )
+    print('my cycle type is ' + cycle)
+def soak(door_is_loocked):
+    if door_is_loocked==True:
+        print("i'am soaking!" )
+    else:
+        print('close door')
+
+def agitate(detergent):
+    print('scrubbing with' ,detergent)
+
+def rinse():
+    print('rinsing')   
+
+def wash_lundry(temp,speed,cycle,door_is_loocked,detergent):
+    set_settings(temp,speed,cycle)
+    soak(door_is_loocked)
+    agitate(detergent)
+    rinse()
+
+wash_lundry(30,700,'cottons',True,'persil')
+
+
+        
+
