cdca415d3f02b2a297b4d9ba9fb7ee6e8d64e871
Leah Itzinger <litzinger@integralytic.com>
2023-01-10 11:33:40 +0200

    08.01

---
+++ oop/3a.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from cw3 import CreditCardStatement , main
+
+# my_credit=ccs()
+# df=my_credit.read('credit_card_bill.txt')
+# my_credit.read_file(df)
+# my_credit.calculate(df)
+main()
